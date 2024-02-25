32f2061a5c035bbb0e34bb26d0ae1c18e8f8d3c0
Stuart Leach <jstuartleach@gmail.com>
2024-02-24 21:37:36 -0500

    Update dependencies and configuration

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,10 @@
 module.exports = function (api) {
-    api.cache(true)
+    api.cache(true);
     return {
         presets: [
             'babel-preset-expo', // '@babel/preset-env',
             '@babel/preset-react',
-            'module:metro-react-native-babel-preset',
+            ['module:metro-react-native-babel-preset', { useTransformReactJSXExperimental: true }],
         ],
         plugins: [
             'nativewind/babel',
@@ -20,5 +20,5 @@ module.exports = function (api) {
             ],
             'react-native-reanimated/plugin',
         ],
-    }
-}
+    };
+};
