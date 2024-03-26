0293fd57314eb4568c55fb58014d7c7ad74d723b
Stuart Leach <jstuartleach@gmail.com>
2024-03-26 12:32:43 -0400

    test

--- screens/MeditationScreen/MeditationTimer.tsx
+++ screens/MeditationScreen/MeditationTimer.tsx
@@ -1,28 +1,28 @@
-import React, {useEffect, useState} from 'react'
-import {Animated, Text, View} from 'react-native'
-import {Circle, Svg} from 'react-native-svg'
-import {activeTheme} from '@/styles/theme'
-import {meditationTimerStyles} from '@/styles/theme'
+import React, { useEffect, useState } from 'react';
+import { Animated, Text, View } from 'react-native';
+import { Circle, Svg } from 'react-native-svg';
+import { activeTheme } from '@/styles/theme';
+import { meditationTimerStyles } from '@/styles/theme';
 
 interface CountdownProps {
     timeLeftInMilliseconds: number;
     totalMilliseconds: number;
     circleDiameter: number;
-    isRunning: boolean,
+    isRunning: boolean;
 }
 
 const Countdown: React.FC<CountdownProps> = (props) => {
-    const {timeLeftInMilliseconds, totalMilliseconds, circleDiameter, isRunning} = props
-    const progress = totalMilliseconds ? timeLeftInMilliseconds / totalMilliseconds : 0
-    const radius = circleDiameter / 2 - 20
-    const [strokeWidth, setStrokeWidth] = useState(10)
-    const circumference = 2 * Math.PI * radius
-    const strokeDashoffset = circumference * (1 - progress)
-    const minutes = Math.floor(timeLeftInMilliseconds / 60000)
-    const seconds = Math.floor((timeLeftInMilliseconds % 60000) / 1000)
-    const displayTime = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`
+    const { timeLeftInMilliseconds, totalMilliseconds, circleDiameter, isRunning } = props;
+    const progress = totalMilliseconds ? timeLeftInMilliseconds / totalMilliseconds : 0;
+    const radius = circleDiameter / 2 - 20;
+    const [strokeWidth, setStrokeWidth] = useState(10);
+    const circumference = 2 * Math.PI * radius;
+    const strokeDashoffset = circumference * (1 - progress);
+    const minutes = Math.floor(timeLeftInMilliseconds / 60000);
+    const seconds = Math.floor((timeLeftInMilliseconds % 60000) / 1000);
+    const displayTime = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
     return (
-        <View style={{justifyContent: 'center', alignItems: 'center'}}>
+        <View style={{ justifyContent: 'center', alignItems: 'center' }}>
             <Svg height={circleDiameter} width={circleDiameter} viewBox={`0 0 220 220`}>
                 <Circle
                     cx='110'
@@ -53,12 +53,21 @@ const Countdown: React.FC<CountdownProps> = (props) => {
                 <Text style={meditationTimerStyles.timeDisplay}>{displayTime}</Text>
             </View>
         </View>
-    )
-}
+    );
+};
 
-export const MeditationTimer = (meditationTimerProps: { height: any; duration: any; playing: any; started: any; timeLeftInMilliseconds: any; timerIsVisible: any; isRunning: any }) => {
-    const {height, duration, playing, started, timeLeftInMilliseconds, timerIsVisible, isRunning} = meditationTimerProps
-    const [opacityAnim] = useState(new Animated.Value(1)) // Start fully visible
+export const MeditationTimer = (meditationTimerProps: {
+    height: any;
+    duration: any;
+    playing: any;
+    started: any;
+    timeLeftInMilliseconds: any;
+    timerIsVisible: any;
+    isRunning: any;
+}) => {
+    const { height, duration, playing, started, timeLeftInMilliseconds, timerIsVisible, isRunning } =
+        meditationTimerProps;
+    const [opacityAnim] = useState(new Animated.Value(1)); // Start fully visible
 
     useEffect(() => {
         if (!timerIsVisible) {
@@ -66,18 +75,18 @@ export const MeditationTimer = (meditationTimerProps: { height: any; duration: a
                 toValue: 0, // Animate to invisible
                 duration: 150,
                 useNativeDriver: true,
-            }).start()
+            }).start();
         } else {
             Animated.timing(opacityAnim, {
                 toValue: 1, // Animate back to fully visible
                 duration: 450,
                 useNativeDriver: true,
-            }).start()
+            }).start();
         }
-    }, [timerIsVisible, opacityAnim])
+    }, [timerIsVisible, opacityAnim]);
 
     return (
-        <Animated.View style={[meditationTimerStyles.meditationTimerContainer, {height}, {opacity: opacityAnim}]}>
+        <Animated.View style={[meditationTimerStyles.meditationTimerContainer, { height }, { opacity: opacityAnim }]}>
             <View style={[meditationTimerStyles.smallContainer]}>
                 <View style={meditationTimerStyles.countdown}>
                     <Countdown
@@ -89,5 +98,5 @@ export const MeditationTimer = (meditationTimerProps: { height: any; duration: a
                 </View>
             </View>
         </Animated.View>
-    )
-}
+    );
+};
